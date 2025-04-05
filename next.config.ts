395e9ebb9ac99abf6448e544a20b4ce606605751
Ingrid Larissa <Larissaorcx@gmail.com>
2025-04-05 14:39:58 -0300

    initial config remote

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {  
+  reactStrictMode: true,
+  webpack(config:any, options:any) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'test-web-component',
+        remotes: {
+          "web-component": `web-component@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+      })
+    );
+
+    return config;
+  },
+}
+
+module.exports = nextConfig  
